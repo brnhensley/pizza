eb7f9ef7f1fd6495f1619dedc7d555bd4177b8f5
Brian Hensley <brnhensley@gmail.com>
2019-04-12 09:25:26 -0700

    create the Pizza object construtor and a prototype to change the value of the 'size'

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business Logic
+function Pizza(size, toppings, price) {
+  this.size = size;
+  this.toppings = [];
+  this.price = price;
+}
+
+// Delete Later
+var myPizza = new Pizza()
+
+Pizza.prototype.sizeCalculator = function (inputSize) {
+  this.size = inputSize
+};
+
+Pizza.prototype.addToppings = function (inputTopping) {
+  this.toppings.push(inputTopping);
+}
+
+Pizza.prototype.findPrice = function () {
+
+};
+
+// User Logic
+$(document).ready(function() {
+
+})

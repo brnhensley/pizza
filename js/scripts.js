2d6143cf4d2cd768c6ef01ac2a79031b7bb20e34
Brian Hensley <brnhensley@gmail.com>
2019-04-12 10:39:05 -0700

    calculate an order total based on topping and size, toppings cost more the larger pizza the user chooses

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
-var myPizza = new Pizza();
+// Business Logic  ------------------------------------
 
-// Business Logic
+// Delete after testing
+var myPizza = new Pizza();
+//
+    console.log(myPizza.toppings.length);
+
 function Pizza(size, toppings, price) {
   this.size = size;
   this.toppings = [];
@@ -19,18 +23,39 @@ Pizza.prototype.addToppings = function (inputTopping) {
 Pizza.prototype.findPrice = function () {
   if (this.size === "Small") {
     this.price = 6;
+    this.price += this.toppings.length;
   } else if (this.size === "Medium") {
     this.price = 10;
-  } else {
+    this.price += (this.toppings.length * 2);
+    console.log(myPizza.price);
+    console.log(this.toppings.length);
+  } else if (this.size === "Large"){
     this.price = 14;
-  }
-  if (this.toppings.length > 0) {
-    this.price += this.toppings.length;
+    this.price += (this.toppings.length * 3);
+  } else {
+    this.price = 40;
+    this.price += (this.toppings.length * 5);
   }
   return this.price;
 };
 
-// User Logic
+// User Interface Logic -------------------------------
 $(document).ready(function() {
-
+  $("form#new-order").submit(function(event) {
+    event.preventDefault();
+    var inputSize = $("select.new-pizza-size").val();
+    var inputTopping = $("").val();
+    var myPizza = new Pizza(inputSize, inputTopping)
+  });
 })
+
+// This function listens for clicks on toppings
+// function attachToppingListeners() {
+//   $()
+//   // On click the toppings should appear on the page in a list under order size.  On submit the price should appear
+// }
+//
+// // Shows the final order to the user
+// function showOrder() {
+//
+// }
